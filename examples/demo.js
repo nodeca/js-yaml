c5ae0bdda4e3b00133450e2d23aee5bc8eb251f2
Aleksey V Zapparov <ixti@member.fsf.org>
2011-10-30 15:16:51 +0100

    Addded dummy example

---
+++ examples/demo.js
@@ -0,0 +1,8 @@
+try {
+  var str = require('fs').readFileSync(__dirname + '/demo.yml', 'utf8'),
+      yaml = require(__dirname + '/../lib/js-yaml');
+  console.log(yaml.load(str));
+} catch (e) {
+  console.log(e.toString());
+  console.log(e.stack);
+}
