3cd9f85885ee042a63e6efc3f01bfe4ed9ae42c0
Aleksey V Zapparov <ixti@member.fsf.org>
2011-10-31 14:18:28 +0100

    Implemented pairs constructor

--- examples/demo.js
+++ examples/demo.js
@@ -1,7 +1,9 @@
 try {
   var str = require('fs').readFileSync(__dirname + '/demo.yml', 'utf8'),
-      yaml = require(__dirname + '/../lib/js-yaml');
-  console.log(yaml.loadAll(str));
+      yaml = require(__dirname + '/../lib/js-yaml'),
+      doc = yaml.loadAll(str).shift();
+  console.log(doc);
+  console.log(doc.pairs);
 } catch (e) {
   console.log(e.toString());
   console.log(e.stack);
