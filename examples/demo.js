c82dfae20312cd7353dac119463d91ff40682e91
Aleksey V Zapparov <ixti@member.fsf.org>
2011-10-31 14:27:52 +0100

    Fixed parser and constructor to understand pairs

--- examples/demo.js
+++ examples/demo.js
@@ -1,9 +1,7 @@
 try {
   var str = require('fs').readFileSync(__dirname + '/demo.yml', 'utf8'),
-      yaml = require(__dirname + '/../lib/js-yaml'),
-      doc = yaml.loadAll(str).shift();
-  console.log(doc);
-  console.log(doc.pairs);
+      yaml = require(__dirname + '/../lib/js-yaml');
+  console.log(yaml.loadAll(str).shift());
 } catch (e) {
   console.log(e.toString());
   console.log(e.stack);
