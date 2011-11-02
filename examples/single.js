1c5950f9a66012e964b77c9a679351f10dbae327
Aleksey V Zapparov <ixti@member.fsf.org>
2011-11-02 02:43:20 +0100

    Simplified example

--- examples/single.js
+++ examples/single.js
@@ -1,18 +1,10 @@
-var fs = require('fs'),
-    inspect = require('util').inspect,
+var inspect = require('util').inspect,
     jsyaml = require(__dirname + '/../lib/js-yaml'),
-    source = __dirname + '/single.yml';
+    doc;
 
 try {
-  fs.open(source, 'r', function (err, fd) {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    var doc = jsyaml.load(fd);
-    console.log(inspect(doc, false, 10));
-  });
+  doc = require(__dirname + '/single.yml');
+  console.log(inspect(doc, false, 10));
 } catch (e) {
   console.log(e.stack || e.toString());
 }
