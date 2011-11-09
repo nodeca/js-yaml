0bd37225e62295dabfa5f61f71b9a05691417352
Aleksey V Zapparov <ixti@member.fsf.org>
2011-11-09 13:58:00 +0100

    Colorized example

--- examples/single.js
+++ examples/single.js
@@ -4,7 +4,7 @@ var inspect = require('util').inspect,
 
 try {
   doc = require(__dirname + '/single.yml');
-  console.log(inspect(doc, false, 10));
+  console.log(inspect(doc, false, 10, true));
 } catch (e) {
   console.log(e.stack || e.toString());
 }
