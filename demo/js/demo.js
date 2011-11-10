8922091aa26b742a27079344539af7a9a1f21950
Aleksey V Zapparov <ixti@member.fsf.org>
2011-11-10 04:02:06 +0100

    Improved demo

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,26 +1,14 @@
 window.runDemo = function runDemo() {
-  var jsyaml = require('/lib/js-yaml'), source, result, initial, history,
-      timer1, timer2;
-
-
-  history = (window.history && window.history.replaceState)
-          ? function (url) { window.history.replaceState({}, 'YAML', url); }
-          : function (url) { window.location = url; };
-
+  var jsyaml = require('/lib/js-yaml'), source, result, initial, permalink,
+      timer1, timer2 = null;
 
   function parse() {
-    var str, base, hash;
+    var str;
 
     try {
       str = source.getValue();
-      hash = base64.encode(str);
-      base = window.location.toString();
 
-      if (-1 !== base.indexOf('#')) {
-        base = base.slice(0, base.indexOf('#'));
-      }
-
-      history(base + '#!/yaml/' + hash);
+      permalink.href = '#!/yaml/' + base64.encode(str);
 
       result.setOption('mode', 'javascript');
       result.setValue(inspect(jsyaml.load(str), false, 10));
@@ -30,18 +18,24 @@ window.runDemo = function runDemo() {
     }
   }
 
+  permalink = document.getElementById('permalink');
+
   source = CodeMirror.fromTextArea(document.getElementById('source'), {
     mode: 'yaml',
     undoDepth: 1,
     onKeyEvent: function (_, evt) {
       if (evt.type != 'keyup') {
         window.clearTimeout(timer1);
-        window.clearTimeout(timer2);
-
         timer1 = window.setTimeout(parse, 500);
-        window.setTimeout(function () {
-          timer2 = window.setTimeout(parse, 1000);
-        });
+
+        if (null === timer2) {
+          timer2 = setTimeout(function () {
+            window.clearTimeout(timer1);
+            window.clearTimeout(timer2);
+            timer2 = null;
+            parse();
+          }, 1000);
+        }
       }
     }
   });
