edb7a891fdc1918d554a7e57cba8d57a87a46b74
Aleksey V Zapparov <ixti@member.fsf.org>
2011-11-11 23:06:09 +0100

    Added issue #17 test case

---
+++ test/issues/issue-17.js
@@ -0,0 +1,11 @@
+var issue = module.exports = {},
+    assert = require('assert'),
+    jsyaml = require(__dirname + '/../../lib/js-yaml'),
+    source = __dirname + '/data/issue-17.yml';
+
+issue.title = "#17: Non-specific `!` tags should resolve to !!str";
+issue.fixed = false;
+issue.execute = function () {
+  var str = require(source).shift();
+  assert.equal('string', typeof str);
+};
