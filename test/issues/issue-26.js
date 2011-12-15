da00d1c1b070ab40d160f67b05b179131be3767f
Aleksey V Zapparov <ixti@member.fsf.org>
2011-12-15 16:09:45 +0100

    Adds tests for #26

---
+++ test/issues/issue-26.js
@@ -0,0 +1,11 @@
+var issue = module.exports = {},
+    assert = require('assert'),
+    jsyaml = require(__dirname + '/../../lib/js-yaml'),
+    source = __dirname + '/data/issue-26.yml';
+
+issue.title = "#26: should convert new line into white space";
+issue.fixed = false;
+issue.execute = function () {
+  var doc = require(source).shift();
+  assert.equal(doc.test, 'a b c');
+};
