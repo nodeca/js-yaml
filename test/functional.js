7d542d01336fd8e90d95f60de380d18f28fff5df
Dervus Grim <dervus@lavabit.com>
2012-12-01 00:13:32 +0100

    Add 'functional/mark' tests.

--- test/functional.js
+++ test/functional.js
@@ -3,6 +3,7 @@
 
 describe('Functional.', function () {
   require('./functional/errors');
+  require('./functional/mark');
   require('./functional/resolver');
   require('./functional/structure');
   require('./functional/tokens');
---
+++ test/functional/mark.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var assert = require('chai').assert;
+var functional = require('../helpers/functional');
+var Mark = require('../../lib/js-yaml/errors').Mark;
+
+
+functional.generateTests({
+  description: 'Test marks.',
+  files: ['.marks'],
+  handler: function (marksFile) {
+    marksFile.data.split('---\n').slice(1).forEach(function (input) {
+      var index = 0, line = 0, column = 0,
+          mark, snippet, data, pointer, temp;
+
+      assert.include(input, '*');
+
+      while (input[index] !== '*') {
+        if (input[index] === '\n') {
+          line += 1;
+          column = 0;
+        } else {
+          column += 1;
+        }
+        index += 1;
+      }
+
+      mark = new Mark(marksFile.path, index, line, column, input, index);
+      snippet = mark.getSnippet(2, 79);
+
+      assert.isString(snippet);
+      
+      temp = snippet.split('\n');
+      assert.lengthOf(temp, 2);
+
+      data = temp[0];
+      pointer = temp[1];
+
+      assert(data.length < 82);
+      assert.strictEqual(data[pointer.length - 1], '*');
+    });
+  }
+});
