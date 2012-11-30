973c9d1f085145346d546185a0606468560fef90
Dervus Grim <dervus@lavabit.com>
2012-12-01 00:08:15 +0100

    Add test suite for functional tests.

---
+++ test/functional.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+describe('Functional.', function () {
+  require('./functional/errors');
+  require('./functional/resolver');
+  require('./functional/structure');
+  require('./functional/tokens');
+});
