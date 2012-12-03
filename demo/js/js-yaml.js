5731ab91bbc3c8061ffa8904e5a227e9841c499e
Aleksey V Zapparov <ixti@member.fsf.org>
2012-12-04 00:59:35 +0100

    Update browserified version

--- demo/js/js-yaml.js
+++ demo/js/js-yaml.js
@@ -20,6 +20,34 @@
  * THE SOFTWARE.
  */
 
+(function () {
+  'use strict';
+
+  // getOwnPropertyNames shim for IE7. Thanks to @Fanjita
+  // Not using es5-shams as they give more problems. See:
+  // https://github.com/nodeca/js-yaml/issues/49#issuecomment-10963606
+  if ('function' !== typeof Object.getOwnPropertyNames) {
+    Object.getOwnPropertyNames = function getOwnPropertyNames(obj) {
+      var keys = [], k;
+
+      // Only iterate the keys if we were given an object, and
+      // a special check for null, as typeof null == "object"
+      if (!!obj && 'object' === typeof obj) {
+        // Use a standard for in loop
+        for (k in obj) {
+          // A for in will iterate over members on the prototype
+          // chain as well, but Object.getOwnPropertyNames returns
+          // only those directly on the object, so use hasOwnProperty.
+          if (obj.hasOwnProperty(k)) {
+            keys.push(k);
+          }
+        }
+      }
+
+      return keys;
+    };
+  }
+}());
 var jsyaml = window.jsyaml = (function () {
 var require = function (file, cwd) {
     var resolved = require.resolve(file, cwd || '/');
