e371cdfc4f43935caae843310bb3a7fb1468de17
Dervus Grim <dervus@lavabit.com>
2012-12-20 19:28:31 +0100

    Add description for `paranoidEqual` function

--- test/support/assert-paranoid-equal.js
+++ test/support/assert-paranoid-equal.js
@@ -6,6 +6,13 @@ var inspectors = require('./assert-paranoid-equal/inspectors');
 var Context = require('./assert-paranoid-equal/context');
 
 
+/*
+ * This function provides a "paranoid" variant of Node's assert.deepEqual
+ * See assert-paranoid-equal's README for details.
+ *
+ * https://npmjs.org/package/assert-paranoid-equal
+ *
+ */
 function paranoidEqual(value1, value2) {
   inspectors.ensureEqual(new Context(value1, value2), value1, value2);
 }
