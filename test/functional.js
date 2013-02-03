a4320a3443ee7fc5d2ec0b529d006eb45ef427d7
Dervus Grim <dervus@lavabit.com>
2013-02-03 17:09:41 +0100

    Add initial dumper code.

--- test/functional.js
+++ test/functional.js
@@ -6,4 +6,5 @@ describe('Functional.', function () {
   require('./functional/loader');
   require('./functional/errors');
   require('./functional/single-errors');
+  require('./functional/dumper');
 });
---
+++ test/functional/dumper.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+var path   = require('path');
+var jsyaml = require('../../lib/js-yaml');
+var helper = require('../support/helper');
+
+var paranoidEqual = require('../support/assert-paranoid-equal').paranoidEqual;
+var TEST_SCHEMA   = require('../support/schema');
+
+
+helper.generateTests({
+  description: 'Dumper.',
+  directory: path.join(__dirname, 'loader'),
+  files: ['.js'],
+  test: function (codeFile) {
+    var object, serialized, deserialized;
+
+    object       = codeFile.content;
+    serialized   = jsyaml.dump(object,     { schema: TEST_SCHEMA });
+    deserialized = jsyaml.load(serialized, { schema: TEST_SCHEMA });
+
+    if ('function' === typeof object) {
+      object.call(this, deserialized);
+    } else {
+      paranoidEqual(deserialized, object);
+    }
+  }
+});
