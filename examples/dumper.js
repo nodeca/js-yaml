f2b3ace220491af0764c07c2dd6f937849b09453
Dervus Grim <dervus@lavabit.com>
2013-02-08 22:16:39 +0100

    Update `dumper` example.

--- examples/dumper.js
+++ examples/dumper.js
@@ -16,16 +16,16 @@ console.log(yaml.dump(object, {
 
 // Output:
 //==============================================================================
-// "name": "Wizzard"
-// "level": 0x11
-// "sanity": Null
-// "inventory":
-//   - "name": "Hat"
-//     "features": ["magic", "pointed"]
-//     "traits": {}
-//   - "name": "Staff"
-//     "features": []
-//     "traits": {"damage": 0xA}
-//   - "name": "Cloak"
-//     "features": ["old"]
-//     "traits": {"defence": 0x0, "comfort": 0x3}
+// name: Wizzard
+// level: 0x11
+// sanity: Null
+// inventory:
+//   - name: Hat
+//     features: [magic, pointed]
+//     traits: {}
+//   - name: Staff
+//     features: []
+//     traits: {damage: 0xA}
+//   - name: Cloak
+//     features: [old]
+//     traits: {defence: 0x0, comfort: 0x3}
