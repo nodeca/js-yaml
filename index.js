d51e167c7daab3b2021ec47cd4b8625645bda1b5
Aleksey V Zapparov <ixti@member.fsf.org>
2013-10-27 01:21:49 +0200

    Move require patch into index

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-module.exports = require('./lib/js-yaml.js');
+'use strict';
+
+
+var fs   = require('fs');
+var yaml = require('./lib/js-yaml.js');
+
+
+require.extensions['.yml'] = require.extensions['.yaml'] = function (m, f) {
+  m.exports = yaml.safeLoad(fs.readFileSync(f, 'utf8'), { filename: f });
+};
+
+
+module.exports = yaml;
