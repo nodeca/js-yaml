c3b1fd4cd13e3f71c27781b30e7d9a118a7d8662
Alex Kocharin <alex@kocharin.ru>
2014-04-17 17:16:57 +0000

    use require('js-yaml/register') to register yaml extensions

---
+++ require.js
@@ -0,0 +1,6 @@
+var fs   = require('fs');
+var yaml = require('./lib/js-yaml.js');
+
+require.extensions['.yml'] = require.extensions['.yaml'] = function (m, f) {
+  m.exports = yaml.safeLoad(fs.readFileSync(f, 'utf8'), { filename: f });
+};
