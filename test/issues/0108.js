05bd18a12fbff952f1ce23ae119e1ad4db88cb81
Dervus Grim <dervus.grim@gmail.com>
2014-07-05 20:11:35 +0200

    Ensure that input always contains the trailing line break
    Related to issue #126

--- test/issues/0108.js
+++ test/issues/0108.js
@@ -6,7 +6,7 @@ var yaml   = require('../../');
 
 
 test('Literal scalars have an unwanted leading line break', function () {
-  assert.strictEqual(yaml.load('|\n  foobar'),              'foobar');
-  assert.strictEqual(yaml.load('|\n  hello\n  world'),      'hello\nworld');
+  assert.strictEqual(yaml.load('|\n  foobar\n'),            'foobar\n');
+  assert.strictEqual(yaml.load('|\n  hello\n  world\n'),      'hello\nworld\n');
   assert.strictEqual(yaml.load('|\n  war never changes\n'), 'war never changes\n');
 });
