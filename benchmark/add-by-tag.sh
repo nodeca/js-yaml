d36d316fe46ee582847db9ad20cc2b300be63104
Dervus Grim <dervus.grim@gmail.com>
2014-12-27 22:46:28 +0100

    Improve benchmark/add-by-tag.sh script

--- benchmark/add-by-tag.sh
+++ benchmark/add-by-tag.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-if test -z "$1"; then
-  echo "No branch/tag name provided"
+if test $# -eq 0; then
+  echo "No branch/tag names provided"
   exit 1
 fi
 
-git clone --branch "$1" --depth 1 https://github.com/nodeca/js-yaml.git "$(dirname $0)/implementations/js-yaml-$1"
+root_dir="$(dirname $0)"
+
+for branch in $@; do
+  impl_dir="$root_dir/implementations/js-yaml-$branch"
+
+  if test -e "$impl_dir"; then
+    echo "$impl_dir already exists; skipping"
+  else
+    git clone --branch "$branch" --depth 1 https://github.com/nodeca/js-yaml.git "$impl_dir"
+  fi
+done
