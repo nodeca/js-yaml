c4a9faba12cfb30498ba74e2d79786299a06bc46
Dina Berry <diberry@microsoft.com>
2019-02-24 13:05:32 -0800

    reformat JSON, remove unneeded property for repro

--- test/issues/0468.js
+++ test/issues/0468.js
@@ -8,13 +8,22 @@ test('should not indent arrays an extra level when disabled', function () {
   /* eslint-disable max-len */
   var output = yaml.dump(
     [
-      { a:'a_val', b:'b_val' },
-      { a:'a2_val', expanded:true, items:
-        [
-          { a:'a_a_val', b:'a_b_val' }
+      {
+        a: 'a_val',
+        b: 'b_val'
+      },
+      {
+        a: 'a2_val',
+        items: [
+          {
+            a: 'a_a_val',
+            b: 'a_b_val'
+          }
         ]
       }
-    ], { noArrayIndent: true });
-  var expected = '- a: a_val\n  b: b_val\n- a: a2_val\n  expanded: true\n  items:\n  - a: a_a_val\n    b: a_b_val\n';
+    ],
+    { noArrayIndent: true }
+  );
+  var expected = '- a: a_val\n  b: b_val\n- a: a2_val\n  items:\n  - a: a_a_val\n    b: a_b_val\n';
   assert.strictEqual(output, expected);
 });
