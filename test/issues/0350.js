59b6e76b712350e9e6aad7773e7375a1de009cf9
Vitaly Puzrin <vitaly@rcdesign.ru>
2019-03-07 04:58:37 +0300

    Fix test name

--- test/issues/0350.js
+++ test/issues/0350.js
@@ -6,7 +6,7 @@ var yaml = require('../../');
 var readFileSync = require('fs').readFileSync;
 
 
-test('should allow cast integers as !!float', function () {
+test('should return parse docs from loadAll', function () {
   var data = yaml.safeLoadAll(readFileSync(require('path').join(__dirname, '/0350.yml'), 'utf8'));
 
   assert.deepEqual(data, [ { a: 1 }, { b: 2 } ]);
