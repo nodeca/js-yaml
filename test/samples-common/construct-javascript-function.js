33977f6ee3a85c1f567c3336061e0df8874dcea3
Scott Deakin <sdeaks@gmail.com>
2019-05-10 08:02:14 +0100

    Added support for async functions

--- test/samples-common/construct-javascript-function.js
+++ test/samples-common/construct-javascript-function.js
@@ -45,6 +45,14 @@ testHandler.expected = [
       second: 'sum',
       third: result
     };
+  },
+  async function (r) {
+    await r;
+    return r + 1;
+  },
+  async function (q) {
+    await q;
+    return q + 1;
   }
 ];
 
