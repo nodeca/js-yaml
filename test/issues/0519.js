2245cd47fbaa5ab46fc1aa03adcfbab3886674ab
Murtaza Jafferji <murtazajafferji@gmail.com>
2019-10-15 11:47:21 -0700

    Fix test by adding newline

--- test/issues/0519.js
+++ test/issues/0519.js
@@ -7,5 +7,5 @@ test('Dumper should add quotes around equals sign', function () {
   // pyyaml fails with unquoted `=`
   // https://yaml-online-parser.appspot.com/?yaml=%3D%0A&type=json
   assert.strictEqual(yaml.load(yaml.dump('=')), '=');
-  assert.strictEqual(yaml.dump('='), "'='");
+  assert.strictEqual(yaml.dump('='), "'='\n");
 });
