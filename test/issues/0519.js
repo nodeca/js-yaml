154013506e09a71246870cfdd64a82a40021a7a2
Murtaza Jafferji <murtazajafferji@gmail.com>
2019-10-14 18:09:20 -0700

    Fix test by adding newline

--- test/issues/0519.js
+++ test/issues/0519.js
@@ -4,7 +4,7 @@ var assert = require('assert');
 var yaml   = require('../../');
 
 test('Loader should not strip quotes for lines containing equals sign', function () {
-  var line_with_equals_sign = "'='";
+  var line_with_equals_sign = "'='\n";
   var result_of_load_and_dump = yaml.dump(yaml.load(line_with_equals_sign));
   assert.strictEqual(line_with_equals_sign, result_of_load_and_dump);
 });
