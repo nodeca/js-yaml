910fb350c8aaa2044ae027dedf6e329e61370b2b
Maxim Gavrilov <maxim.gavrilov@gmail.com>
2020-01-28 21:45:56 +0300

    Fix noArrayIndent on nested arrays

--- test/issues/0432.js
+++ test/issues/0432.js
@@ -16,3 +16,9 @@ test('should not indent arrays an extra level when disabled', function () {
   var expected = 'array:\n- a\n- b\n';
   assert.strictEqual(output, expected);
 });
+
+test('should always indent nested arrays', function () {
+  var output = yaml.safeDump({ array: [ 'a', [ 'b', 'c' ], 'd' ] }, { noArrayIndent: true });
+  var expected = 'array:\n- a\n- - b\n  - c\n- d\n';
+  assert.strictEqual(output, expected);
+});
