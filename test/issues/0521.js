91f9662250a55ec30ffa2948da6c331efbe2364d
Himura Kazuto <glagol15@gmail.com>
2020-06-16 18:25:45 +0300

    Improve Plain Style support
    close https://github.com/nodeca/js-yaml/pull/557

--- test/issues/0521.js
+++ test/issues/0521.js
@@ -6,21 +6,22 @@ var yaml = require('../../');
 
 
 it('Don\'t quote strings with # without need', function () {
-  var data = yaml.load(`
-http://example.com/page#anchor: no#quotes#required
+  var required = `
+http://example.com/page#anchor: no:quotes#required
 parameter#fallback: 'quotes #required'
+'quotes: required': Visit [link](http://example.com/foo#bar)
 'foo #bar': key is quoted
-`);
+`.replace(/^\n/, '');
 
   var sample = {
-    'http://example.com/page#anchor': 'no#quotes#required',
+    'http://example.com/page#anchor': 'no:quotes#required',
     'parameter#fallback': 'quotes #required',
+    'quotes: required': 'Visit [link](http://example.com/foo#bar)',
     'foo #bar': 'key is quoted'
   };
 
-  assert.deepStrictEqual(
+  assert.strictEqual(
     yaml.dump(sample),
-    yaml.dump(data)
+    required
   );
-
 });
