a6de32cc2321952ddc9d9455519d86f5f054ffb8
Yann Savary <yann@amplify.ooo>
2020-11-20 20:31:31 +0100

    Fix and tests for https://github.com/nodeca/js-yaml/issues/587

---
+++ test/issues/0587.js
@@ -0,0 +1,24 @@
+'use strict';
+
+
+var assert = require('assert');
+var yaml = require('../../');
+
+
+suite('Should not escape emojis', function () {
+  test('plain', function () {
+    assert.strictEqual(yaml.safeDump('😀'), '😀\n');
+  });
+
+  test('escape \\ and " in double-quoted', function () {
+    assert.strictEqual(yaml.safeDump('\u0007 😀 escape\\ escaper"'), '"\\a 😀 escape\\\\ escaper\\""\n');
+  });
+
+  test('escape non-printables', function () {
+    assert.strictEqual(yaml.safeDump('a😀\nb\u0001c'), '"a😀\\nb\\x01c"\n');
+  });
+
+  test('load', function () {
+    assert.strictEqual(yaml.safeLoad('"\\U0001F600"'), '😀');
+  });
+});
