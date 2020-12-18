38528f7a7e46fc0a94d4154785344f40d70341e4
Alex Kocharin <alex@kocharin.ru>
2020-12-18 14:01:25 +0300

    Fix minor issue in formatting error snippets

--- test/issues/0332.js
+++ test/issues/0332.js
@@ -15,4 +15,26 @@ it('Should format errors', function () {
  1 | "foo\u0001bar"
 -------------^`);
   }
+
+  try {
+    yaml.load('*');
+  } catch (err) {
+    assert.strictEqual(err.toString(), `YAMLException: name of an alias node must contain at least one character (1:2)
+
+ 1 | *
+------^`);
+  }
+
+  try {
+    yaml.load('foo:\n  bar: 1\na');
+  } catch (err) {
+    // eslint-disable-next-line max-len
+    assert.strictEqual(err.toString(), `YAMLException: can not read a block mapping entry; a multiline key may not be an implicit key (4:1)
+
+ 1 | foo:
+ 2 |   bar: 1
+ 3 | a
+ 4 | 
+-----^`);
+  }
 });
