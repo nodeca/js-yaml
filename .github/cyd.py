1dc8d4ecfe0f17f4a15996463e3af8abcb0b2c56
Saeid Askari <askari.saeed@gmail.com>
2021-03-25 15:19:58 -0300

    changed pipeline

--- .github/cyd.py
+++ .github/cyd.py
@@ -4,17 +4,21 @@ import time
 
 base_url = 'https://redtest.ca/api/'
 
-response = requests.post(f'{base_url}users/login/', data={'username': os.getenv('CYDERIAN_USERNAME'), 'password': os.getenv('CYDERIAN_PASSWORD')})
+response = requests.post(f'{base_url}users/login/', data={
+    'username': os.getenv('CYDERIAN_USERNAME'), 'password': os.getenv('CYDERIAN_PASSWORD')
+})
 access = response.json()['access']
 headers = {'Authorization': f'Bearer {access}'}
-print(access)
 
 response = requests.get(f'{base_url}users/self', headers=headers)
 user_id = response.json()['id']
-print(user_id)
 
-response = requests.post(f'{base_url}users/{user_id}/git/', data={'repo': os.getenv('REPO'),
-                                               'commit': os.getenv('GITHUB_SHA')},headers=headers)
+response = requests.post(f'{base_url}users/{user_id}/git/',
+                         data={
+                          'repo': os.getenv('REPO'),
+                          'commit': os.getenv('GITHUB_SHA'),
+                          'project': os.getenv('PROJECT_ID'),
+                         }, headers=headers)
 
 analysis_id = response.json()['analysis']
 
@@ -24,10 +28,9 @@ while True:
   if data['total_crashes'] > 0:
     response = requests.get(f'{base_url}analyses/{analysis_id}/crashes', headers=headers)
     for crash in response.json():
-      print(f"Crash Stack Trace: {crash['stackTrace']}")
-    print(f'Analysis Page: https://redtest.ca/{os.getenv("GITHUB_REPOSITORY").split("/")[-1]}/{analysis_id}/report')
+      print(crash['stackTrace'])
+    print('Found crashes visit https://redtest.ca to download crashes.')
     exit(1)
   if data['status'] == 'T':
     exit(0)
   time.sleep(3)
-
