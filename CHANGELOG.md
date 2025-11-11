383665ff4248ec2192d1274e934462bb30426879
Alex Kocharin <alex@kocharin.ru>
2025-11-11 04:07:26 +0400

    fix prototype pollution in merge (<<)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,11 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 
+## [4.1.1] - 2025-11-12
+### Security
+- Fix prototype pollution issue in yaml merge (<<) operator.
+
+
 ## [4.1.0] - 2021-04-15
 ### Added
 - Types are now exported as `yaml.types.XXX`.
--- test/issues/0164.js
+++ test/issues/0164.js
@@ -6,9 +6,25 @@ const yaml = require('../../');
 
 
 it('should define __proto__ as a value (not invoke setter)', function () {
-  let object = yaml.load('{ __proto__: {foo: bar} }');
+  let object = yaml.load('{ __proto__: {polluted: bar} }');
 
   assert.strictEqual(({}).hasOwnProperty.call(yaml.load('{}'), '__proto__'), false);
   assert.strictEqual(({}).hasOwnProperty.call(object, '__proto__'), true);
-  assert(!object.foo);
+  assert(!object.polluted);
+});
+
+
+it('should merge __proto__ as a value with << operator', function () {
+  let object = yaml.load(`
+payload: &ref
+  polluted: bar
+
+foo:
+  <<:
+    __proto__: *ref
+  `);
+
+  assert.strictEqual(({}).hasOwnProperty.call(yaml.load('{}'), '__proto__'), false);
+  assert.strictEqual(({}).hasOwnProperty.call(object.foo, '__proto__'), true);
+  assert(!object.foo.polluted);
 });
