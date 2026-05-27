b2710223288925352aa9b6e2521b26a323e014a1
Vitaly Puzrin <vitaly@rcopen.com>
2026-05-27 06:20:45 +0300

    Revert "fix & test: throw error on zero-indentation block scalar (close #280)…"
    This reverts commit 3d7d34aeb2bc27dc386dc5ace6d5600c5ac1d67a.

--- test/issues/0144.js
+++ test/issues/0144.js
@@ -1,10 +1,10 @@
 'use strict';
 
+
 var assert = require('assert');
 var yaml   = require('../../');
 
 
 it('Infinite loop when attempting to parse multi-line scalar document that is not indented', function () {
-  // Block scalar with zero indentation should throw an error (issue #280)
-  assert.throws(() => yaml.load('--- |\nfoo\n'), /missing indentation for block scalar/);
+  assert.strictEqual(yaml.load('--- |\nfoo\n'), 'foo\n');
 });
