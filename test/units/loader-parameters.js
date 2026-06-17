9b5b6c6e4bae9d788c2ff7c8fe2009331d0fd55a
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2026-06-17 01:15:49 +0000

    test: add coverage

--- test/units/loader-parameters.js
+++ test/units/loader-parameters.js
@@ -52,3 +52,50 @@ suite('loader parameters', function () {
     assert.deepEqual(result, expected);
   });
 });
+
+// Builds a YAML string with a merge sequence of `count` anchored mappings
+function buildMergeSeqYaml(count) {
+  var repeated = '*a, '.repeat(count);
+
+  return [
+    'a: &a { k: 0 }',
+    'b: { <<: [ ' + repeated + '*a ] }'
+  ].join('\n');
+}
+
+suite('maxMergeSeqLength option', function () {
+  test('merge sequence within default limit (20) should succeed', function () {
+    var input = buildMergeSeqYaml(19);
+    assert.doesNotThrow(function () {
+      yaml.safeLoad(input);
+    });
+  });
+
+  test('merge sequence exceeding default limit (20) should throw', function () {
+    var input = buildMergeSeqYaml(20);
+    assert.throws(function () {
+      yaml.safeLoad(input);
+    }, /merge sequence length exceeded maxMergeSeqLength \(20\)/);
+  });
+
+  test('custom maxMergeSeqLength allows longer sequences', function () {
+    var input = buildMergeSeqYaml(29);
+    assert.doesNotThrow(function () {
+      yaml.safeLoad(input, { maxMergeSeqLength: 30 });
+    });
+  });
+
+  test('custom maxMergeSeqLength rejects sequences that exceed it', function () {
+    var input = buildMergeSeqYaml(4);
+    assert.throws(function () {
+      yaml.safeLoad(input, { maxMergeSeqLength: 4 });
+    }, /merge sequence length exceeded maxMergeSeqLength \(4\)/);
+  });
+
+  test('merge sequence within custom limit should succeed', function () {
+    var input = buildMergeSeqYaml(3);
+    assert.doesNotThrow(function () {
+      yaml.safeLoad(input, { maxMergeSeqLength: 4 });
+    });
+  });
+});
